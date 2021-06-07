51bb047cb285de097c489d37ec520bee2bf0c362
Rishabh <31581762+Rishabh1803@users.noreply.github.com>
2021-06-07 23:18:02 +0530

    Delete Exercise4 directory

--- Exercise4/src/com/rk/workers/DailyWorker.java
+++
@@ -1,8 +0,0 @@
-package com.rk.workers;
-
-public class DailyWorker extends Worker{
-	public void Pay(int hours) {
-		setSalaryrate(10);
-		System.out.println("Daily Worker get paid: " + (getSalaryrate() * hours));
-	}
-}
--- Exercise4/src/com/rk/workers/SalariedWorker.java
+++
@@ -1,8 +0,0 @@
-package com.rk.workers;
-
-public class SalariedWorker extends Worker{
-	public void Pay(int hours) {
-		setSalaryrate(15);
-		System.out.println("Salaried Worker gets paid: " + (getSalaryrate() * 40));
-	}
-}
