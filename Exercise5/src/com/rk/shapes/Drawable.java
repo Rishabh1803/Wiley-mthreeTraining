1fbd88479ba98c353bb91d06ff3f0f13036dd485
Rishabh <31581762+Rishabh1803@users.noreply.github.com>
2021-06-07 23:18:12 +0530

    Delete Exercise5 directory

--- Exercise5/src/com/rk/shapes/Drawable.java
+++
@@ -1,6 +0,0 @@
-package com.rk.shapes;
-
-public interface Drawable {
-	public void drawingColor();
-	public void thickness();
-}
--- Exercise5/src/com/rk/shapes/Fillable.java
+++
@@ -1,6 +0,0 @@
-package com.rk.shapes;
-
-public interface Fillable {
-	public void fillingColor();
-	public void size();
-}
